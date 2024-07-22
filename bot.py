3f4528371cb2139d831feff31cfdaebc76f14a48
AniShin21 <minecraft2727k@gmail.com>
2024-07-22 20:24:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(
                     "Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(
-                    f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
+                    f"Please Double check the FORCE_SUB_CHANNEL_3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
                 self.LOGGER(__name__).info(
                     "\nBot Stopped. Contact @iTz_Anayokoji for support")
                 sys.exit()
@@ -90,7 +90,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(
                     "Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(
-                    f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
+                    f"Please Double check the FORCE_SUB_CHANNEL_4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
                 self.LOGGER(__name__).info(
                     "\nBot Stopped. Contact @iTz_Anayokoji for support")
                 sys.exit()
